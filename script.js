505dea5b7c7ca40cee7400eb6c8b444edf8ac07a
Nikita88888 <74507770+Nikita88888@users.noreply.github.com>
2021-08-15 13:20:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio =  (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+
+
+//mein
+
+
+//fullscreen
+
+fullscreen = false
+
+
+window.addEventListener('keydown', (f) => {
+
+    if (fullscreen === false) {
+
+        console.log("Fullscreen = on")
+
+        document.documentElement.requestFullscreen()
+
+        fullscreen = true
+    }
+    else if (fullscreen === true) {
+
+        console.log("Fullscreen = off")
+
+        document.exitFullscreen()
+
+        fullscreen = false
+    }
+
+})
